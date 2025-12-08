4f45f185ff3d5d6e321e8babbb93385abf9bcf31
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2025-12-08 23:48:30 +0200

    depVercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      
+      "backend.wecandevmode.online",
     ], // add imgur domain here
   },
   eslint: {
