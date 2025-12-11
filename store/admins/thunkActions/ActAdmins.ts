ffa65dbe3bb4fe8648c04973ace4c1b860bdb1e0
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2025-12-11 23:00:28 +0200

    createAdmins

--- store/admins/thunkActions/ActAdmins.ts
+++ store/admins/thunkActions/ActAdmins.ts
@@ -71,36 +71,3 @@ export const ActToggleAdminStatus = createAsyncThunk<IAdmin, { id: number; is_ac
     }
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import api from "@/services/api";
-
-// export const ActFetchAdmins = createAsyncThunk(
-//   "admins/fetch",
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const res = await api.get("/admins");
-
-//       // Keep the structure as: [{ name, controls: [] }, ...]
-//       const grouped = res.data.data.map((group: any) => ({
-//         name: group.name,
-//         controls: group.controls ?? []
-//       }));
-
-//       return grouped;
-//     } catch (err: any) {
-//       return rejectWithValue(err?.response?.data?.message ?? err.message);
-//     }
-//   }
-// );
